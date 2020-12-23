a1cf34a5b1a726d2727bd9bd29d5adb5245d52a4
Valentin Mihov <valentin.mihov@gmail.com>
2020-12-23 15:58:27 +0200

    Added memoization

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,30 +1,46 @@
 from brownie import interface
 from brownie.exceptions import ContractNotFound
 from datetime import datetime, timedelta
+from functools import lru_cache
 
 DAI = interface.ERC20("0x6b175474e89094c44da98b954eedeac495271d0f")
 WETH = interface.ERC20("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
 USDC = interface.ERC20("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
-UNISWAP_ROUTER = interface.UniswapRouterV2("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_FACTORY = interface.UniswapFactoryV2("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")
 
-def getReserves(token, otherToken):
+@lru_cache
+def getFactory(router):
+  return interface.UniswapFactoryV2(router.factory())
+
+@lru_cache
+def getToken0(pair):
+  return interface.ERC20(pair.token0())
+
+@lru_cache
+def getToken1(pair):
+  return interface.ERC20(pair.token1())
+
+@lru_cache
+def getPair(factory, token0, token1):
+  return interface.UniswapPair(factory.getPair(token0, token1))
+
+def getReserves(token, otherToken, factory):
   try:
-    pair = interface.UniswapPair(UNISWAP_FACTORY.getPair(token, otherToken))
+    pair = getPair(factory, token, otherToken)
   except ContractNotFound:
     return 0
 
   (token0Reserves, token1Reserves, _) = pair.getReserves()
 
-  if token == pair.token0():
+  if token == getToken0(pair):
     return token0Reserves
   else:
     return token1Reserves
 
-def getUSDCPath(token):
-  reservesInWETH = getReserves(token, WETH)
-  reservesInUSDC = getReserves(token, USDC)
-  reservesInDAI = getReserves(token, DAI)
+def getUSDCPath(token, router):
+  factory = getFactory(router)
+  reservesInWETH = getReserves(token, WETH, factory)
+  reservesInUSDC = getReserves(token, USDC, factory)
+  reservesInDAI = getReserves(token, DAI, factory)
 
   maxReserves = max(reservesInDAI, reservesInUSDC, reservesInWETH)
 
@@ -36,16 +52,19 @@ def getUSDCPath(token):
 
   return [token, USDC]
 
-def priceOf(token):
-  return UNISWAP_ROUTER.getAmountsOut(10 ** token.decimals(), getUSDCPath(token))[-1] / 10 ** USDC.decimals()
+def priceOf(token, router_address="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"):
+  router = interface.UniswapRouterV2(router_address)
+  return router.getAmountsOut(10 ** token.decimals(), getUSDCPath(token, router))[-1] / 10 ** USDC.decimals()
 
-def priceOfUniPair(uni_pair):
+def priceOfUniPair(uni_pair, router_address="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"):
+  router = interface.UniswapRouterV2(router_address)
+
   (token0Reserves, token1Reserves, _) = uni_pair.getReserves()
-  token0 = interface.ERC20(uni_pair.token0())
-  token1 = interface.ERC20(uni_pair.token1())
+  token0 = getToken0(uni_pair)
+  token1 = getToken1(uni_pair)
 
-  token0Price = priceOf(token0)
-  token1Price = priceOf(token1)
+  token0Price = priceOf(token0, router)
+  token1Price = priceOf(token1, router)
 
   total_pool = token0Reserves * token0Price / 10 ** token0.decimals() + token1Reserves * token1Price / 10 ** token1.decimals()
 
