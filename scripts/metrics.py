6411880129a38144ecbc70395737183680e548f6
Valentin Mihov <valentin.mihov@gmail.com>
2021-02-24 22:13:25 +0200

    Add all the stable pools in Viking

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -7,7 +7,9 @@ from scripts.utils import priceOf
 import scripts.master_chef as master_chef
 
 VIKING_MASTER_CHEF = interface.VikingMasterChef("0xEf6e807fD2c0Ef5883A03Ed1b962333E8C9b725f")
-VIKING_TOKEN = interface.IERC20("0xc15fa3e22c912a276550f3e5fe3b0deb87b55acd")
+PANCAKE_USDT_BUSD = interface.IERC20("0xc15fa3e22c912a276550f3e5fe3b0deb87b55acd")
+PANCAKE_DAI_BUSD = interface.IERC20("0x3ab77e40340ab084c3e23be8e5a6f7afed9d41dc")
+PANCAKE_USDC_BUSD = interface.IERC20("0x680dd100e4b394bda26a59dd5c119a391e747d18")
 PANCAKESWAP_ROUTER = "0x05ff2b0db69458a0750badebc4f9e13add608c7f"
 
 TOKEN_PRICES = [
@@ -31,13 +33,41 @@ MASTER_CHEF_FARMS = [
     ),
     [
       VIKING_MASTER_CHEF,
-      VIKING_TOKEN,
+      PANCAKE_USDT_BUSD,
       "viking",
       "vikingPerBlock",
       3,
       PANCAKESWAP_ROUTER
     ]
-  )
+  ),
+  (
+    (
+      Gauge("farm_viking_tvl_dai_busd", "Viking USDT/BUSD TVL"),
+      Gauge("farm_viking_apr_dai_busd", "Viking USDT/BUSD APR")
+    ),
+    [
+      VIKING_MASTER_CHEF,
+      PANCAKE_DAI_BUSD,
+      "viking",
+      "vikingPerBlock",
+      3,
+      PANCAKESWAP_ROUTER
+    ]
+  ),
+  (
+    (
+      Gauge("farm_viking_tvl_usdc_busd", "Viking USDT/BUSD TVL"),
+      Gauge("farm_viking_apr_usdc_busd", "Viking USDT/BUSD APR")
+    ),
+    [
+      VIKING_MASTER_CHEF,
+      PANCAKE_USDC_BUSD,
+      "viking",
+      "vikingPerBlock",
+      3,
+      PANCAKESWAP_ROUTER
+    ]
+  ),
 ]
 
 def update_metrics():
