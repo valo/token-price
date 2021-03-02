28e0ff3a6c592052f305b7843cf2e86a07e2c25b
Valentin Mihov <valentin.mihov@gmail.com>
2021-03-03 01:40:39 +0200

    Add the needed functions to the 1inch pair interface

--- interfaces/IMooniswap.sol
+++ interfaces/IMooniswap.sol
@@ -8,4 +8,8 @@ interface IMooniswap {
     function getTokens() external view returns (IERC20[] memory tokens);
 
     function tokens(uint256 i) external view returns (IERC20);
+
+    function totalSupply() external view returns (uint256);
+
+    function decimals() external view returns (uint8);
 }
