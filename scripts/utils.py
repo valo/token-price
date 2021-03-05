2e8d8188bfc052d8cd4eda80e8e5e5da9e338cc6
Valentin Mihov <valentin.mihov@gmail.com>
2021-03-05 17:57:44 +0200

    Lazy load the contrats in utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,10 +4,21 @@ from brownie.exceptions import ContractNotFound
 from datetime import datetime, timedelta
 from functools import lru_cache
 
-DAI = interface.IERC20(os.environ.get('DAI_ADDRESS', "0x6b175474e89094c44da98b954eedeac495271d0f"))
-WETH = interface.IERC20(os.environ.get('WETH_ADDRESS', "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"))
-USDT = interface.IERC20(os.environ.get('USDT_ADDRESS', "0xdac17f958d2ee523a2206206994597c13d831ec7"))
-USDC = interface.IERC20(os.environ.get('USDC_ADDRESS', "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"))
+@lru_cache
+def DAI():
+  return interface.IERC20(os.environ.get('DAI_ADDRESS', "0x6b175474e89094c44da98b954eedeac495271d0f"))
+
+@lru_cache
+def WETH():
+  return interface.IERC20(os.environ.get('WETH_ADDRESS', "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"))
+
+@lru_cache
+def USDT():
+  return interface.IERC20(os.environ.get('USDT_ADDRESS', "0xdac17f958d2ee523a2206206994597c13d831ec7"))
+
+@lru_cache
+def USDC():
+  return interface.IERC20(os.environ.get('USDC_ADDRESS', "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"))
 
 @lru_cache
 def getFactory(router):
@@ -40,30 +51,30 @@ def getReserves(token, otherToken, factory):
 
 def getUSDCPath(token: interface.IERC20, router: interface.UniswapRouterV2):
   factory = getFactory(router)
-  if token != WETH:
-    reservesInWETH = getReserves(WETH, token, factory) * priceOf(WETH, router)
+  if token != WETH():
+    reservesInWETH = getReserves(WETH(), token, factory) * priceOf(WETH(), router)
   else:
     reservesInWETH = 0
 
-  reservesInUSDT = getReserves(USDT, token, factory)
-  reservesInUSDC = getReserves(USDC, token, factory)
-  reservesInDAI = getReserves(DAI, token, factory)
+  reservesInUSDT = getReserves(USDT(), token, factory)
+  reservesInUSDC = getReserves(USDC(), token, factory)
+  reservesInDAI = getReserves(DAI(), token, factory)
 
   maxReserves = max(reservesInDAI, reservesInUSDC, reservesInWETH, reservesInUSDT)
 
   if reservesInDAI == maxReserves:
-    return [token, DAI]
+    return [token, DAI()]
   
   if reservesInWETH == maxReserves:
-    return [token, WETH, USDC]
+    return [token, WETH(), USDC()]
 
   if reservesInUSDT == maxReserves:
-    return [token, USDT]
+    return [token, USDT()]
 
-  return [token, USDC]
+  return [token, USDC()]
 
 def priceOf(token: interface.IERC20, router_address: str):
-  if token == USDC or token == USDT:
+  if token == USDC() or token == USDT():
     return 1.0
 
   router = interface.UniswapRouterV2(router_address)
@@ -95,14 +106,14 @@ def priceOf1InchPair(oneinch_pair: interface.IMooniswap, router_address: str):
   (token0, token1) = oneinch_pair.getTokens()
 
   if token0 == "0x0000000000000000000000000000000000000000":
-    token0 = WETH
+    token0 = WETH()
     token0Reserves = oneinch_pair.balance()
   else:
     token0 = interface.IERC20(token0)
     token0Reserves = token0.balanceOf(oneinch_pair)
 
   if token1 == "0x0000000000000000000000000000000000000000":
-    token1 = WETH
+    token1 = WETH()
     token1Reserves = oneinch_pair.balance()
   else:
     token1 = interface.IERC20(token1)
