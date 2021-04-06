f71fb9b14e2f7e68a4456238c812d0b43a814c4d
Valentin Mihov <valentin.mihov@gmail.com>
2021-04-06 11:44:17 +0300

    Add Heco blockchain support

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -26,6 +26,9 @@ def update_metrics():
   elif NETWORK == "ethereum":
     from .networks.ethereum import MASTER_CHEF_FARMS, TOKEN_PRICES, STAKING_REWARDS_FARMS
     BLOCK_TIME = 15
+  elif NETWORK == "heco":
+    from .networks.heco import MASTER_CHEF_FARMS, TOKEN_PRICES, STAKING_REWARDS_FARMS
+    BLOCK_TIME = 3
 
   def update_prices():
     for dex in TOKEN_PRICES():
