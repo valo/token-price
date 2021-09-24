7d22911bb0e4a8a8101388db9dace29439dc323b
Valentin Mihov <valentin.mihov@gmail.com>
2021-09-24 12:52:00 +0300

    Add MIM/ETH pool

--- scripts/networks/arbitrum.py
+++ scripts/networks/arbitrum.py
@@ -7,11 +7,13 @@ ETH = interface.IERC20("0x82af49447d8a07e3bd95bd0d56f35241523fbab1")
 BTC = interface.IERC20("0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f")
 USDC = interface.IERC20("0xff970a61a04b1ca14834a43f5de4533ebddb5cc8")
 USDT = interface.IERC20("0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9")
+MIM = interface.IERC20("0xfea7a6a0b346362bf88a9e4a88416b77a57d6c2a")
 
 @lru_cache()
 def TOKEN_PRICES():
   SUSHI_USDC_ETH = interface.UniswapPair("0x905dfcd5649217c42684f23958568e533c711aa3")
   SUSHI_USDT_ETH = interface.UniswapPair("0xcb0e5bfa72bbb4d16ab5aa0c60601c438f04b4ad")
+  SUSHI_MIM_ETH = interface.UniswapPair("0xb6dd51d5425861c808fd60827ab6cfbffe604959")
 
   return {
   "Sushi-Arbitrum": (
@@ -19,8 +21,10 @@ def TOKEN_PRICES():
     [
       ("ETH", ETH),
       ("BTC", BTC),
+      ("MIM", MIM),
       ("USDC_ETH", SUSHI_USDC_ETH),
       ("USDT_ETH", SUSHI_USDT_ETH),
+      ("MIM_ETH", SUSHI_MIM_ETH),
     ]
   ),
 }
