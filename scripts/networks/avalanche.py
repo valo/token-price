0d0958101be3268cc2fdbbb4960750ffedaa5750
Valentin Mihov <valentin.mihov@gmail.com>
2021-11-23 14:09:33 +0200

    Add AVAX price to the avalanche tracking

--- scripts/networks/avalanche.py
+++ scripts/networks/avalanche.py
@@ -2,12 +2,14 @@ from brownie import interface
 from functools import lru_cache
 
 TRADERJOE_ROUTER = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4"
+PANGOLIN_ROUTER = "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106"
 
 ETH = interface.IERC20("0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB")
 BTC = interface.IERC20("0x50b7545627a5162f82a992c33b87adc75187b218")
 USDC = interface.IERC20("0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664")
 USDT = interface.IERC20("0xc7198437980c041c805a1edcba50c1ce5db95118")
 TIME = interface.IERC20("0xb54f16fb19478766a268f172c9480f8da1a7c9c3")
+AVAX = interface.IERC20("0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7")
 
 @lru_cache()
 def TOKEN_PRICES():
@@ -18,6 +20,12 @@ def TOKEN_PRICES():
       ("ETH", ETH),
       ("BTC", BTC),
       ("TIME", TIME),
+    ]
+  ),
+  "Pangolin": (
+    PANGOLIN_ROUTER,
+    [
+      ("AVAX", AVAX),
     ]
   ),
 }
