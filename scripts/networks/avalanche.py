8d7418f361fdbc259519d1e8d2c584b08c83713e
Valentin Mihov <valentin.mihov@gmail.com>
2021-12-22 12:33:04 +0200

    Add Tesseract avalanche vaults

--- scripts/networks/avalanche.py
+++ scripts/networks/avalanche.py
@@ -11,6 +11,12 @@ USDT = interface.IERC20("0xc7198437980c041c805a1edcba50c1ce5db95118")
 TIME = interface.IERC20("0xb54f16fb19478766a268f172c9480f8da1a7c9c3")
 AVAX = interface.IERC20("0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7")
 
+tVaultUSDC = interface.yEarnVault("0x52cE2c4Bd817AdB765c476901cc09621DCACEc62")
+tVaultDAI = interface.yEarnVault("0xdC808bADe323205f2c794198C1adDa8aEF215E29")
+tVaultBTC = interface.yEarnVault("0x4AaC7D14E674BCE476eD387FF5abb0Ac3F3187a1")
+tVaultETH = interface.yEarnVault("0x7f99CC27EF01F0d32939dBd993f6a857B6e9d235")
+tVaultAVAX = interface.yEarnVault("0x83EA27549acc3CB64c3fCda8379d1eA229a02712")
+
 @lru_cache()
 def TOKEN_PRICES():
   return {
@@ -20,6 +26,12 @@ def TOKEN_PRICES():
       ("ETH", ETH),
       ("BTC", BTC),
       ("TIME", TIME),
+
+      ("tvUSDC", tVaultUSDC),
+      ("tvDAI", tVaultDAI),
+      ("tvWBTC", tVaultBTC),
+      ("tvWETH", tVaultETH),
+      ("tvWAVAX", tVaultAVAX),
     ]
   ),
   "Pangolin": (
