7a7afc8c1e62e45a3e02e7a30110504a3692f79e
Valentin Mihov <valentin.mihov@gmail.com>
2022-02-04 13:18:43 +0200

    Add stables tracking for AVAX

--- scripts/networks/avalanche.py
+++ scripts/networks/avalanche.py
@@ -8,6 +8,7 @@ ETH = interface.IERC20("0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB")
 BTC = interface.IERC20("0x50b7545627a5162f82a992c33b87adc75187b218")
 USDC = interface.IERC20("0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664")
 USDT = interface.IERC20("0xc7198437980c041c805a1edcba50c1ce5db95118")
+DAI = interface.IERC20("0xd586e7f844cea2f87f50152665bcbc2c279d8d70")
 TIME = interface.IERC20("0xb54f16fb19478766a268f172c9480f8da1a7c9c3")
 AVAX = interface.IERC20("0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7")
 
@@ -26,6 +27,9 @@ def TOKEN_PRICES():
     [
       ("WETH.e", ETH),
       ("WBTC.e", BTC),
+      ("USDC.e", USDC),
+      ("USDT.e", USDT),
+      ("DAI.e", DAI),
       ("TIME", TIME),
 
       ("tvUSDC.e", tVaultUSDC),
